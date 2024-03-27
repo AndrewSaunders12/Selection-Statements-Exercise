ce05770f3d87293628257b5f770092b4cd815dd5
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-03-27 14:55:57 -0400

    If statements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            int favNumber = r.Next(1, 1000);
+            Console.WriteLine("Guess my favorite number between 1 in 1000:");
+            var userInput = int.Parse(Console.ReadLine());
+
+            
+            if(favNumber > userInput)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if(favNumber < userInput)
+            {
+                Console.WriteLine("Too High");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
