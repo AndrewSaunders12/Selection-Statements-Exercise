a73381e84f8439442781818ace85bcb5f418fa40
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-10-15 18:09:37 -0400

    Selection statements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNum = 13;
+
+            Console.WriteLine("Guess my favorite number:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > favNum)
+            {
+                Console.WriteLine("Too high");
+                
+            }
+            else if (userInput < favNum)
+            {
+                Console.WriteLine("Too low");
+            }
+            else 
+            { 
+                Console.WriteLine("You guessed it!!");
+            }
+
         }
     }
 }
